ef564d83d9f0b67ad6701082d5b58c38ab7cc994
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-02-19 16:28:00 +0530

    Create ViewWrapper.js

---
+++ src/components/ViewWrapper.js
@@ -0,0 +1,42 @@
+import React from "react";
+import CalendarView from "./Calendar";
+import DayView from "./DayView";
+import MonthView from "./MonthView";
+import WeekView from "./WeekView";
+import "./ViewWrapper.css";
+import { useSelector } from "react-redux";
+
+export default function ViewWrapper() {
+  const render = useSelector((arg) => arg.mode);
+
+  const renderComponent = () => {
+    let rtr;
+    switch (render.mode) {
+      case "day":
+        rtr = <DayView />;
+        break;
+      case "week":
+        rtr = <WeekView />;
+        break;
+      case "month":
+        rtr = <MonthView />;
+        break;
+      default:
+        rtr = <DayView />;
+        break;
+    }
+    return rtr;
+  };
+
+  // console.log(renderComponent)
+  return (
+    <div className="view_wrapper">
+      {render.calendarVisible && (
+        <div className="wt1">
+          <CalendarView />
+        </div>
+      )}
+      {renderComponent()}
+    </div>
+  );
+}
