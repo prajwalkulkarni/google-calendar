ff1964bad35f7b8dc98309dd827e6697108b815b
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-02-20 15:32:29 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import { dateSlice, modeSlice, eventSlice } from "./slices";
+import { dateSlice, modeSlice, eventSlice, monthTaskSlice } from "./slices";
 const store = configureStore({
   reducer: {
     date: dateSlice.reducer,
     mode: modeSlice.reducer,
-    task: eventSlice.reducer
+    task: eventSlice.reducer,
+    monthtask: monthTaskSlice.reducer
   }
 });
 
