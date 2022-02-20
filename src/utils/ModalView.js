3e148685c2eb302b8e958aa949143fd7941227e4
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-02-20 21:13:43 +0530

    Update ModalView.js

--- src/utils/ModalView.js
+++ src/utils/ModalView.js
@@ -10,7 +10,7 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Stack from "@mui/material/Stack";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { timeRows } from "./data";
 const style = {
